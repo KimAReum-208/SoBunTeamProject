c0238aeafcbbfb093dc3c2b1e74cbb1c4a4bcbae
BangDaeHo <hanava1@naber.com>
2019-11-23 16:07:02 +0900

    Library 클래스 내부 껍데기 작성

---
+++ Library.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Library here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Library
+{
+    private String name;
+    
+    public void ReturnOneBook(int caralogueNumber){
+        
+    }
+    
+    public void RegisterOneBook(String name){
+        
+    }
+    
+    public void RegisterOneBorrower(String name){
+        
+    }
+    
+    public void LendOneBook(String name, int catalogueNumber){
+        
+    }
+    
+    public book DisplayBooksInLoan(){
+        return book;
+    }
+    
+    public book DisplayBooksForLoan(){
+        return book;
+    }
+    
+    public String toString(){
+        this.name = name;
+    }
+}
