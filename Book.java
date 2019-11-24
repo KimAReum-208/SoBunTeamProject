848709f95b0965e863bed91402200f3823a81e1a
choisewon <chltpdnjs5@naver.com>
2019-11-24 17:08:59 +0900

    Book 클래스를 만들었습니다.

---
+++ Book.java
@@ -0,0 +1,79 @@
+
+/**
+ * 여기에 Book 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class Book
+{
+    private static int catalogueNumber;
+    private String title;
+    private String author;
+    TreeSet tree = new TreeSet(); // 북 리스트가 담겨져있는 컬렉션
+    
+    public Book(int catalogueNumber,String title,String author)
+    {
+        this.catalogueNumber = catalogueNumber;
+        this.title = title;
+        this.author = author;
+    }
+    
+    public void add(Book book)
+    {
+        tree.add(this);
+    }
+
+    public Book finBook(int catalogueNumber)
+    {
+        if (this.catalogueNumber != catalogueNumber)
+        {
+            return this;
+        }
+    }
+
+    public Loan chekBook()
+    {
+        
+        return loan;
+    }
+
+    public void offConnect()
+    {
+        loan = null;
+    }
+
+    public Book MakeBook()
+    {
+        // ?
+    }
+
+    public void connect(Loan laon)
+    {
+        laon = this;
+    }
+
+    public void display()
+    {
+        while(tree.hasNext())
+        {
+            if(Loan.BorrowerdBook != null)
+                System.out.println(tree);
+            else if (Loan.borrower == null)
+                System.out.println(tree);
+        }
+    }
+
+    public String toString()
+    {
+        
+    }
+
+    public void CheckBook(int catalogueNumber)
+    {
+        if (this.catalogueNumber != catalogueNumber)
+        {
+            System.out.println("ok");
+        }
+    }
+}
