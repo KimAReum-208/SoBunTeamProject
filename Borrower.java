f98cc5451a27e759cae4daaeb848aec4e8bb5de7
areum <rladkfma2030@gmail.com>
2019-11-24 18:18:54 +0900

    Borrower 클래스 생성 및 메소드 작성

---
+++ Borrower.java
@@ -0,0 +1,39 @@
+import java.util.*;
+/**
+ * 여기에 Borrower 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class Borrower
+{
+    private String name;
+    Set hashSet = new HashSet();
+    public Borrower(String name){
+        this.name = name;
+    }
+    
+    public Borrower getBorrower(String name){
+        return Borrower borrower;
+    }
+    
+    public Borrower MakeBorrower(String name){
+        return Borrower borrower;
+    }
+    
+    public void add(Borrower borrower){
+        hashSet.add(borrower);
+    }
+    
+    public void connect(Loan loan){
+        
+    }
+    
+    public void offConnect(Loan loan){
+        
+    }
+    
+    public String toString(){
+        return this.name;
+    }
+}
