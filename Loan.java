3002d6f10b7015ac08f28709d6ae961db6d713b6
SuhEunyeong <eunyeong317@gmail.com>
2019-11-24 17:12:05 +0900

    loan작성

---
+++ Loan.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Ioan here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Loan
+{
+    private laonDate Date;
+    private returnDate Date;
+    
+    public LocalDate getLoanDate(){
+        
+    }
+    
+    public LocalDate ReturnDate(){
+        
+    }
+    
+    public String toString(){
+        
+    }
+}
